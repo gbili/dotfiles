aa5fa9e6a78c183db00e79268e28d2f6096ac130
Ubuntu <ubuntu@ov-36cd34.infomaniak.ch>
2020-06-12 18:51:27 +0000

    feat: integrate docker-compose template

--- .bash_aliases
+++ .bash_aliases
@@ -1,4 +1,6 @@
 alias link-dotfiles='bash ./scripts/link-dotfiles.bash' # not very usefull, once linked no need to execute this
-alias create-wordpress-app='bash ./scripts/create-wordpress-app.bash'
-alias cwa-dup='bash ./scripts/wpdup.bash'
-alias cwa-clean='bash ./scripts/wpclean.bash'
+
+# Docker wordpress
+alias create-wordpress-app='bash ./scripts/create-wordpress-app/create-wordpress-app.bash'
+alias cwa-dup='bash ./scripts/create-wordpress-app/wpdup.bash'
+alias cwa-clean='bash ./scripts/create-wordpress-app/wpclean.bash'
--- scripts/create-wordpress-app/create-wordpress-app.bash
+++ scripts/create-wordpress-app/create-wordpress-app.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-[ $# -eq 0 ] && { echo "Usage: $0 -r <reverse_domain> -d <host.domain> -p <wp_db_password> -m <mysql_root_password>"; exit 1; }
+[ $# -eq 0 ] && { echo "Usage: $0 -r <reverse_domain> -d <host.domain> -p <wp_db_password> -m <mysql_root_password> -s <dockerfile_sites_dir>"; exit 1; }
 
 # get the parameter -d's value
-while getopts d:m:p:r: flag
+while getopts d:m:p:r:s flag
 do
     case "${flag}" in
         d) hostdomain=${OPTARG};;
         m) mysqlrootpassword=${OPTARG};;
         p) wpdbpassword=${OPTARG};;
         r) reversedomain=${OPTARG};;
+        s) sitesdir=${OPTARG};;
     esac
 done
 
@@ -29,8 +30,22 @@ if [ -z "$wpdbpassword" ]; then
     echo "missing parameter -p <wp_db_password>";
     exit -1;
 fi
+if [ -z "$sitesdir" ]; then
+    sitesdir="$HOME/Documents/workspace/sites"
+    echo "No sites dir specified with option -s, using default";
+    echo "$sitesdir";
+fi
 
-sitesdir="/home/ubuntu/Documents/workspace/sites"
+dockertmplname="docker-compose.yml.wordpress.tmpl"
+dockertmpl="$sitesdir/$dockertmplname"
+
+if [ ! -f "$dockertmpl" ]; then
+    currdir="$(dirname "$(readlink -f "$0")")"
+    echo "Missing docker-compose.yml template, looking for: "$dockertmpl"";
+    dockertmpl="$currdir/create-wordpress-app/$dockertmplname";
+    echo "Using default in $dockertmpl";
+fi
+
 domaindir="$sitesdir/$reversedomain"
 
 # cd to dir and create if not exists
@@ -47,9 +62,8 @@ if [ ! -d "$domaindir" ]; then
 fi
 
 cd $domaindir;
-
 # prepare dir for docker-compose
-cp "$sitesdir/docker-compose.yml.wordpress.tmpl" "$domaindir/docker-compose.yml";
+cp "$dockertmpl" "$domaindir/docker-compose.yml";
 sed -i -e "s/REVERSE_DOMAIN/$reversedomain/g" "$domaindir/docker-compose.yml"
 sed -i -e "s/DOMAIN.TLD/$hostdomain/g" "$domaindir/docker-compose.yml"
 sed -i -e "s/WPDBPASSWORD/$wpdbpassword/g" "$domaindir/docker-compose.yml"
